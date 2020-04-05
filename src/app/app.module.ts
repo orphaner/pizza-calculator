ffd8eb10b4009bfcec71ea4e2fb4479b728fc5ad
Frédérique Milc <hello@milc.fr>
2020-04-05 13:11:32 +0200

    styles: fix imports of *bootstrap for performance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
 import { NgModule } from '@angular/core';
-import { TabsModule } from 'ngx-bootstrap';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { AppComponent } from './app.component';
 
 @NgModule({
